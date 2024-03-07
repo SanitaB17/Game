42f4694aaf6b3857d76a98474c05704defb3cbe3
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-03-07 22:58:21 +0000

    add ascii art and main menu

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -19,4 +19,51 @@ sales = SHEET.worksheet('bookings')
 
 data = sales.get_all_values()
 
-print(data)
+
+def main_menu():
+    # ASCII art title
+    title = """
+,------.                  ,--.      ,--------.             ,--.         
+|  .--. ' ,---. ,--,--, ,-'  '-.    '--.  .--',---.  ,---. |  | ,--,--. 
+|  '--'.'| .-. :|      \\'-.  .-'       |  |  | .-. :(  .-' |  |' ,-.  | 
+|  |\\  \\ \\   --.|  ||  |  |  |         |  |  \\   --..-'  `)|  |\\ '-'  | 
+`--' '--' `----'`--''--'  `--'         `--'   `----'`----' `--' `--`--' 
+                ,----.     ,--.   ,--.     ,---.                        
+                '.-.  |     \  `.'  /     '   .-'                       
+                  .' <       '.    /      `.  `-.                       
+                /'-'  |        |  |       .-'    |                      
+                `----'         `--'       `-----'                       
+    """
+    # Main menu options
+    menu_options = [
+        "1. Rent Car",
+        "2. View Booking",
+        "3. Cancel Booking"
+    ]
+
+    # Display the ASCII art title
+    print(title)
+
+    # Display the main menu options
+    for option in menu_options:
+        print(option)
+
+    # Get user input for menu selection
+    choice = input("Please enter your choice: ")
+
+    if choice == "1":
+        print("You chose Option Rent Car")
+    elif choice == "2":
+        print("You chose View Booking")
+    elif choice == "3":
+        print("You chose Cancel Booking")
+        return
+    else:
+        print("Invalid choice. Please try again.")
+
+    # Recursive call to display the main menu again
+    main_menu()
+
+
+# Call the main menu function to start the program
+main_menu()
