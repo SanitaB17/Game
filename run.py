347973c9d6783e8a444db053e57b8420b56e0d32
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-03-10 13:17:52 +0000

    fix errors

--- run.py
+++ run.py
@@ -165,16 +165,17 @@ def view_booking():
     Search for a name and prints the booking details
     """
     search_n = input("Please enter your name: ")
-    
+
     bookings = sales.get_all_values()
-    
+
     m_bookings = [booking for booking in bookings if booking[0] == search_n]
-    
+
     if not m_bookings:
         print(f"No bookings found for {search_n}")
     else:
         print("Bookings found:")
         for booking in m_bookings:
             print(booking)
+
 
 main_menu()
